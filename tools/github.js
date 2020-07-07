ec50b42ffef1dd7137eb8f6c354f977e28ca86fd
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2020-07-07 17:34:25 +0000

    swap out python `requests` for js `request` lib, also adjust `post` request call appropriately in `tools/github`

--- tools/github.js
+++ tools/github.js
@@ -1,11 +1,11 @@
-import * as requests from 'requests';
+import request from 'request';
 import {join} from 'os/path';
 
 function create_deployment(repo_slug, oauth_token, environment, ref = "master") {
-    return requests.post({"url": join("https://api.github.com/repos", repo_slug, "deployments"), "headers": {"Accept": "application/vnd.github.ant-man-preview+json", "Authorization": `token ${oauth_token}`}, "json": {"ref": ref, "environment": environment, "required_contexts": []}}).json();
+    return request.post({"url": join("https://api.github.com/repos", repo_slug, "deployments"), "headers": {"Accept": "application/vnd.github.ant-man-preview+json", "Authorization": `token ${oauth_token}`}, "json": true, "body": {"ref": ref, "environment": environment, "required_contexts": []}});
 }
 function create_deployment_status(repo_slug, oauth_token, deployment_id, state, environment, environment_url = null) {
-    return requests.post({"url": join("https://api.github.com/repos", repo_slug, "deployments", deployment_id.toString(), "statuses"), "headers": {"Accept": "application/vnd.github.flash-preview+json", "Authorization": `token ${oauth_token}`}, "json": {"state": state, "environment": environment, "environment_url": environment_url}}).json();
+    return request.post({"url": join("https://api.github.com/repos", repo_slug, "deployments", deployment_id.toString(), "statuses"), "headers": {"Accept": "application/vnd.github.flash-preview+json", "Authorization": `token ${oauth_token}`}, "json": true, "body": {"state": state, "environment": environment, "environment_url": environment_url}});
 }
 export {create_deployment, create_deployment_status};
 
