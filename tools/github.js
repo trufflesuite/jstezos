c2258b2581819c8c75606413b89800091b501734
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2020-07-07 17:36:15 +0000

    adjust import ref in `tools/github`

--- tools/github.js
+++ tools/github.js
@@ -1,5 +1,5 @@
 import request from 'request';
-import {join} from 'os/path';
+import {join} from 'path';
 
 function create_deployment(repo_slug, oauth_token, environment, ref = "master") {
     return request.post({"url": join("https://api.github.com/repos", repo_slug, "deployments"), "headers": {"Accept": "application/vnd.github.ant-man-preview+json", "Authorization": `token ${oauth_token}`}, "json": true, "body": {"ref": ref, "environment": environment, "required_contexts": []}});
